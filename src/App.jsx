925d8faa6c57c05ae66c4b8d04742ba5e68046ff
Yes-said <bobsaid358@gmail.com>
2024-03-31 12:26:32 +0300

    Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Componets/Navbar';
+import React from 'react';
+import Navbar from './Components/Navbar';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+        <h1>Welcome to Student Result Management System</h1>
+        {/* Your main content goes here */}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">Student Result Management System</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="students">Students</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="courses">Courses</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="result">Results</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
