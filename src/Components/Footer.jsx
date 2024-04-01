469273db37a5b7792a6a0559196b2e29d0a55d8e
Yes-said <bobsaid358@gmail.com>
2024-04-01 16:26:13 +0300

    Footer updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,23 +6,22 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer__content">
-            <span>Company Name</span>
           </div>
           <div className="footer__links">
             <h3>Quick Links</h3>
             <ul>
-              <li><a href="#">Home</a></li>
+              <li><a href="#/">Home</a></li>
               <li><a href="#">About</a></li>
               <li><a href="#">Services</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
           </div>
           <div className="footer__contact">
-            <h3>Contact Us</h3>
-            <p>123 Street Name</p>
-            <p>City, Country</p>
-            <p>Email: info@example.com</p>
-            <p>Phone: +1234567890</p>
+            
+          <p>Off Mombasa road</p>
+            <p>Nairobi Kenya</p>
+            <p>Email: info@kimc.ac.ke</p>
+            <p>Telephone:  +254-020-699 7000</p>
           </div>
         
       </div>
